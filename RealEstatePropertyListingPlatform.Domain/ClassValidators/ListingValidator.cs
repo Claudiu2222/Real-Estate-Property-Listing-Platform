ea43fb8f33ca733b76cb7503a3179b96ada04742
AndreiGalan <andreigalan03@gmail.com>
2023-11-07 14:13:07 +0200

    Added Listing and User

---
+++ RealEstatePropertyListingPlatform.Domain/ClassValidators/ListingValidator.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealEstatePropertyListingPlatform.Domain.Records;
+
+namespace RealEstatePropertyListingPlatform.Domain.ClassValidators
+{
+    public class ListingValidator
+    {
+        private static readonly int MaxStringLength = 100;
+
+        public static string ValidateListing(string title, Money price, string description, List<string> photos,
+                       DateTime dateCreated)
+        {
+
+            string currentError;
+            if ((currentError = ValidateString(title, nameof(title))) != null)
+            {
+                return currentError;
+            }
+
+            if ((currentError = ValidateMoney(price)) != null)
+            {
+                return currentError;
+            }
+
+            if ((currentError = ValidateString(description, nameof(description))) != null)
+            {
+                return currentError;
+            }
+
+            if ((currentError = ValidatePhotos(photos)) != null)
+            {
+                return currentError;
+            }
+
+            if ((currentError = ValidateDateCreated(dateCreated)) != null)
+            {
+                return currentError;
+            }
+
+
+            return currentError!;
+        }
+
+        private static string ValidateMoney(Money price)
+        {
+            if(price.Amount < 0)
+            {
+                return $"Price cannot be negative";
+            }
+
+            if(price.Currency.Length != 3)
+            {
+                return $"Currency must be 3 characters";
+            }
+            return null!;
+        }
+
+        private static string ValidateString(string value, string propertyName)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return $"{propertyName} cannot be empty";
+            }
+
+            if (value.Length > MaxStringLength)
+            {
+                return $"{propertyName} cannot be longer than {MaxStringLength} characters";
+            }
+
+            return null!;
+        }
+
+        private static string ValidatePhotos(List<string> photos)
+        {
+            if (photos.Count == 0)
+            {
+                return $"Photos cannot be empty";
+            }
+
+            return null!;
+        }
+
+        private static string ValidateDateCreated(DateTime dateCreated)
+        {
+
+            if(dateCreated > DateTime.Now)
+            {
+                return $"Date Created cannot be in the future";
+            }
+
+            if(dateCreated < DateTime.Now)
+            {
+                return $"Date Created cannot be in the past";
+            }
+
+            return null!;
+        }
+
+    }
+}
--- RealEstatePropertyListingPlatform.Domain/Entities/Listing.cs
+++ RealEstatePropertyListingPlatform.Domain/Entities/Listing.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RealEstatePropertyListingPlatform.Domain.ClassValidators;
 using RealEstatePropertyListingPlatform.Domain.Common;
 using RealEstatePropertyListingPlatform.Domain.Records;
 
@@ -12,16 +13,42 @@ namespace RealEstatePropertyListingPlatform.Domain.Entities
     {
         public Guid ListingId { get; private set; }
         public Guid ListingCreatorId { get; private set; }
+        public Guid PropertyId { get; private set; }
+        public string Title { get; private set; }
         public Money Price { get; private set; }
+        public string Description { get; private set; }
+        public List<string> Photos { get; private set; }
+        public DateTime DateCreated { get; private set; }
+        public DateTime DateUpdated { get; private set; }
+        public bool Negotiable { get; private set; }
 
-        private Listing()
+
+
+        private Listing(){ }
+
+        public static Result<Listing> Create(Guid listingCreatorId, Guid propertyId, string title, Money price, string description, List<string> photos, DateTime dateCreated, bool negotiable)
         {
+            string error = ListingValidator.ValidateListing(title, price, description, photos, dateCreated);
 
-        }
+            if (string.IsNullOrWhiteSpace(error))
+            {
+                return Result<Listing>.Failure(error);
+            }
 
-        public static Result<Listing> Create(Guid id, Money price)
-        {
-           throw new NotImplementedException();
+            var listing = new Listing
+            {
+                ListingId = new Guid(),
+                ListingCreatorId = listingCreatorId,
+                PropertyId = propertyId,
+                Title = title,
+                Price = price,
+                Description = description,
+                Photos = photos,
+                DateCreated = dateCreated,
+                Negotiable = negotiable
+            };
+
+            return Result<Listing>.Success(listing);
         }
     }
 }
