ce2e73288a6070845a4536f293cda25efe068185
sorodocosmin <61987774+sorodocosmin@users.noreply.github.com>
2023-11-07 23:12:20 +0200

    Auto stash before merge of "main" and "origin/main"

--- RealEstatePropertyListingPlatform/RealEstatePropertyListingPlatform.Application/Persistence/IAsyncRepository.cs
+++ RealEstatePropertyListingPlatform/RealEstatePropertyListingPlatform.Application/Persistence/IAsyncRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RealEstatePropertyListingPlatform.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace RealEstatePropertyListingPlatform.Application.Persistence
 {
-    internal interface IAsyncInterface
+    public interface IAsyncRepository<T> where T : class
     {
+        Task<Result<T>> UpdateAsync(T entity);
+        Task<Result<T>> FindByIdAsync(Guid id);
+        Task<Result<T>> AddAsync(T entity);
+        Task<Result<T>> DeleteAsync(Guid id);
+        Task<Result<IReadOnlyList<T>>> GetPagedReponseAsync(int page, int size);
     }
 }
