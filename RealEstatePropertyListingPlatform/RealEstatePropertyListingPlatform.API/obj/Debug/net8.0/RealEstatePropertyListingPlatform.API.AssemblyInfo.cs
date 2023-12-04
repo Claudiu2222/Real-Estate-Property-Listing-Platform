558f88584ec081829b72a3a92ae573f72de43b0c
Claudiu Popoiu <claupop2002@gmail.com>
2023-12-04 16:36:38 +0200

    Auto stash before merge of "main" and "origin/main"

--- RealEstatePropertyListingPlatform/RealEstatePropertyListingPlatform.API/obj/Debug/net8.0/RealEstatePropertyListingPlatform.API.AssemblyInfo.cs
+++ RealEstatePropertyListingPlatform/RealEstatePropertyListingPlatform.API/obj/Debug/net8.0/RealEstatePropertyListingPlatform.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstatePropertyListingPlatform.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18096315a57be99d79293b137afed905ca547975")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstatePropertyListingPlatform.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstatePropertyListingPlatform.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
